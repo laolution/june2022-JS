86b04dbdb1dd6f8191a5ab57f4c843b93841c650
laolution <laolution@gmail.com>
2022-07-25 19:06:10 +0300

    less 1

---
+++ homework/less1hw/main.js
@@ -0,0 +1,41 @@
+let a, b, c, d, e, f, g, h, i, j, k, l, m;
+a = 'hello ';
+b = 'owu ';
+c = 'com';
+d = 'ua';
+e = 1;
+f = 10;
+g = -999;
+h = 123;
+i = 3.14;
+j = 2.7;
+k = 16;
+l = true;
+m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e, f, g, h, i, j, k)
+console.log(l);
+console.log(m);
+
+alert(a + b + c);
+
+document.write(a + b + c);
+
+
+let firstName = 'Andriy ';
+let midName = 'Orestovych';
+let lastName = 'Lastovetsky ';
+let person = lastName + firstName + midName;
+console.log(person);
+
+a = 100;
+b = '100';
+c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
