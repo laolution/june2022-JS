6af8c464a3d58de65b8195e172dc1c9448fa4088
laolution <laolution@gmail.com>
2022-08-31 22:56:54 +0300

    try nout

--- lesson 9/homework/JS_main_9.js
+++ lesson 9/homework/JS_main_9.js
@@ -2,3 +2,14 @@
 // ілівьсівс
 // \івщсшьілвсьіьсльі
 // іслідлвслівсьі
+// sfjvnfsjdkfvlks
+// 4skjdncvsjdvnsdmldsvlsd,vsds
+// vdflkfbdbs
+// k
+// bsgknbs
+//
+// kgbns;bkg
+// switch (kgbskkd;kgb;kgb
+// gfbk;f;
+// breakfbg
+// fgb;kl)
